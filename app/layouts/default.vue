b5cf32b8fd5fb647abfd8b3ce332a216debbdac4
Samuel Solomon <samuelsolomon452@gmail.com>
2024-11-10 13:09:31 +0300

    Initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <slot />
+    <Footer1 />
+    <Footer2 />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header />
+    <div class="flex">
+      <!-- Left Column: Main Content -->
+      <div class="flex-1">
+        <CourseInfo />
+        <Related />
+        <CourceCont />
+        <Desc />
+        <Instruct />
+        <Review />
+        <More />
+      </div>
+
+      <!-- Right Column: Vertical Sidebar -->
+      <div class="w-1/3 sticky top-0 h-screen">
+        <Vertical />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
